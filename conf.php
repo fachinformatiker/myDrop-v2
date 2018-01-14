f622d8bb5eb49edacee310258c51471fb8ed076e
Patrick Szalewicz <info@mrpopopolski.info>
2018-01-14 12:02:08 +0100

    new function

--- conf.php
+++ conf.php
@@ -1,8 +1,8 @@
 <?php
 
-	$kamera = "22";
-	$ventil1 = "12";
-	$ventil2 = "16";
-	$blitz = "29";
+	$kamera = '22';
+	$ventil1 = '12';
+	$ventil2 = '16';
+	$blitz = '29';
 
 ?>
--- scripts/action.php
+++ scripts/action.php
@@ -1,17 +1,17 @@
 <?php
-	include ("../conf.php");
+	include ('../conf.php');
 	
 		$tropfenzahl = 2; //pro Ventil
 	
-		$x_v1_stack = array("0");
+		$x_v1_stack = array('0');
 		array_push($x_v1_stack, $_GET['v1_d1'], $_GET['v1_w2'], $_GET['v1_d2'], $_GET['v1_w3']);
-		$save_v1_stack = implode(";", $x_v1_stack);
+		$save_v1_stack = implode(';', $x_v1_stack);
 		foreach ($x_v1_stack as $v1_value) {
 		$v1_stack[] = $v1_value * 1000; }
 
-		$x_v2_stack = array("0");
+		$x_v2_stack = array('0');
 		array_push($x_v2_stack, $_GET['v2_d1'], $_GET['v2_w2'], $_GET['v2_d2'], $_GET['v2_w3']);
-		$save_v2_stack = implode(";", $x_v2_stack);
+		$save_v2_stack = implode(';', $x_v2_stack);
 		foreach ($x_v2_stack as $v2_value) {
 		$v2_stack[] = $v2_value * 1000; }
 
@@ -19,42 +19,42 @@
 		$firstwait = $x_firstwait * 1000;
 		$x_lastwait = $_GET['lastwait'];
 		$lastwait = $x_lastwait * 1000;
-		$content = $x_firstwait . ";" . $x_lastwait;
-		$x_blitzdauer = "100";
+		$content = $x_firstwait . ';' . $x_lastwait;
+		$x_blitzdauer = '100';
 		$blitzdauer = $x_blitzdauer * 1000;
 		
 		$file = fopen('../settings/my_settings.txt', 'w');
 		fwrite($file, $content);
 		fclose($file);
 
-		shell_exec("gpio -1 mode ". $kamera  ." out");
-		shell_exec("gpio -1 mode ". $ventil1  ." out");
-		shell_exec("gpio -1 mode ". $blitz  ." out");
+		shell_exec('gpio -1 mode '. $kamera  .' out');
+		shell_exec('gpio -1 mode '. $ventil1  .' out');
+		shell_exec('gpio -1 mode '. $blitz  .' out');
 		
 		//print_r($_GET);
 		
 if ($_GET['ventile'] == '1') {
 	
-		shell_exec("gpio -1 write ". $kamera ." 1");
+		shell_exec('gpio -1 write '. $kamera .' 1');
 		usleep($firstwait);
 		
 		$v1_value = 1;
 		
 for($i = 1; $i <= $tropfenzahl; $i++) {
-		shell_exec("gpio -1 write ". $ventil1 ." 1");
+		shell_exec('gpio -1 write '. $ventil1 .' 1');
 		//echo $v1_stack[$v1_value];
 		usleep($v1_stack[$v1_value]);
 		$v1_value++;
-		shell_exec("gpio -1 write ". $ventil1 ." 0");
+		shell_exec('gpio -1 write '. $ventil1 .' 0');
 		//echo $v1_stack[$v1_value];
 		usleep($v1_stack[$v1_value]);
 		$v1_value++;
 }
-		shell_exec("gpio -1 write ". $blitz ." 1");
+		shell_exec('gpio -1 write '. $blitz .' 1');
 		usleep($blitzdauer);
-		shell_exec("gpio -1 write ". $blitz ." 0");
+		shell_exec('gpio -1 write '. $blitz .' 0');
 		usleep($lastwait);
-		shell_exec("gpio -1 write ". $kamera ." 0");
+		shell_exec('gpio -1 write '. $kamera .' 0');
 	
 		$v1_file = fopen('../settings/v1_my_settings.txt', 'w');
 		fwrite($v1_file, $save_v1_stack);
@@ -66,36 +66,36 @@ for($i = 1; $i <= $tropfenzahl; $i++) {
 
 elseif ($_GET['ventile'] == '2') {
 	
-		shell_exec("gpio -1 write ". $kamera ." 1");
+		shell_exec('gpio -1 write '. $kamera .' 1');
 		usleep($firstwait);
 		
 		$v1_value = 1;
 		$v2_value = 1;
 		
 for($i = 1; $i <= $tropfenzahl; $i++) {
-		shell_exec("gpio -1 write ". $ventil1 ." 1");
+		shell_exec('gpio -1 write '. $ventil1 .' 1');
 		//echo $v1_stack[$v1_value];
 		usleep($v1_stack[$v1_value]);
 		$v1_value++;
-		shell_exec("gpio -1 write ". $ventil1 ." 0");
+		shell_exec('gpio -1 write '. $ventil1 .' 0');
 		//echo $v1_stack[$v1_value];
 		usleep($v1_stack[$v1_value]);
 		$v1_value++;
-		shell_exec("gpio -1 write ". $ventil2 ." 1");
+		shell_exec('gpio -1 write '. $ventil2 .' 1');
 		//echo $v2_stack[$v2_value];
 		usleep($v2_stack[$v2_value]);
 		$v2_value++;
-		shell_exec("gpio -1 write ". $ventil2 ." 0");
+		shell_exec('gpio -1 write '. $ventil2 .' 0');
 		//echo $v2_stack[$v2_value];
 		usleep($v2_stack[$v2_value]);
 		$v2_value++;
 }
 		
-		shell_exec("gpio -1 write ". $blitz ." 1");
+		shell_exec('gpio -1 write '. $blitz .' 1');
 		usleep($blitzdauer);
-		shell_exec("gpio -1 write ". $blitz ." 0");
+		shell_exec('gpio -1 write '. $blitz .' 0');
 		usleep($lastwait);
-		shell_exec("gpio -1 write ". $kamera ." 0");
+		shell_exec('gpio -1 write '. $kamera .' 0');
 	
 		$v1_file = fopen('../settings/v1_my_settings.txt', 'w');
 		fwrite($v1_file, $save_v1_stack);
@@ -108,6 +108,6 @@ for($i = 1; $i <= $tropfenzahl; $i++) {
 		header('Location: ' . $_SERVER['HTTP_REFERER']);
 }
 else {
-		echo "<center><h1>Fehler</h1></center>";
+		echo '<center><h1>Fehler</h1></center>';
 }
 ?>
